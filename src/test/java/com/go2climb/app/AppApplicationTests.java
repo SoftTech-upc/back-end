4c39369bdc3b3aab29a8ac7b83663109b5b204f1
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-05-21 21:15:38 -0500

    Initial commit

---
+++ src/test/java/com/go2climb/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.go2climb.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
