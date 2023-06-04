14331c8d7a8dba99253c605a9afbe8ef9b4dd677
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-06-03 20:10:38 -0500

    Add activity service

---
+++ src/main/java/com/go2climb/app/activity/service/ActivityServiceImpl.java
@@ -0,0 +1,55 @@
+package com.go2climb.app.activity.service;
+
+import com.go2climb.app.activity.domain.service.ActivityService;
+import com.go2climb.app.activity.domain.model.entity.Activity;
+import com.go2climb.app.activity.domain.persistence.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ActivityServiceImpl implements ActivityService {
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Activity> getAll() {
+        return activityRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Optional<Activity> getById(Integer id) {
+        if (activityRepository.existsById(id)) {
+            return activityRepository.findById(id);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    @Transactional
+    @Override
+    public Activity save(Activity activity) {
+        return activityRepository.save(activity);
+    }
+
+    @Transactional
+    @Override
+    public Activity update(Activity activity) {
+        return activityRepository.save(activity);
+    }
+
+    @Transactional
+    @Override
+    public boolean deleteById(Integer id) {
+        if (activityRepository.existsById(id)) {
+            activityRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
