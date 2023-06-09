b616cbda4f0e909b1bf2459c67526d6dba136d59
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-06-09 08:00:27 -0500

    Revert "Feature/exceptions"

--- src/main/java/com/go2climb/app/shared/constant/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.go2climb.app.shared.constant;
-
-public class Constant {
-  public final static String AGENCY_ENTITY = "Agency";
-  public final static String ACTIVITY_ENTITY = "Activity";
-  public final static String TOUR_ENTITY = "Tour";
-  public final static String TOURIST_ENTITY = "Tourist";
-}
