39118cbf47c527d4fde189848d72f00d8ce4f4a3
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-06-09 11:51:11 -0500

    Fix tour review mapping

--- src/main/java/com/go2climb/app/toursreviews/mapping/MappingConfiguration.java
+++ src/main/java/com/go2climb/app/toursreviews/mapping/MappingConfiguration.java
@@ -1,7 +1,7 @@
 package com.go2climb.app.toursreviews.mapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-@Configuration("learningMappingConfiguration")
+@Configuration("tourReviewMappingConfiguration")
 public class MappingConfiguration {
 
     @Bean
