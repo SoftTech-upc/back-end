e04d3187d4601c9a7c39760cd031ffd132aec88c
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-06-15 07:45:03 -0500

    Fix agency relations

--- src/main/java/com/go2climb/app/agency/domain/model/entity/Agency.java
+++ src/main/java/com/go2climb/app/agency/domain/model/entity/Agency.java
@@ -2,6 +2,7 @@ package com.go2climb.app.agency.domain.model.entity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.go2climb.app.activity.domain.model.entity.Activity;
 import com.go2climb.app.tour.domain.model.entity.Tour;
+import com.go2climb.app.tourist.domain.model.entity.Tourist;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
@@ -19,37 +20,37 @@ public class Agency {
     private Integer id;
 
     @Size(min = 1, max = 30)
-    //@NotNull
+    @NotNull
     @Column(name = "name", length = 30, nullable = false)
     private String name;
 
     @Size(min = 1, max = 100)
-    //@NotNull
+    @NotNull
     @Column(name = "email", length = 100)
     private String email;
 
     @Size(min = 9, max = 9)
-    //@NotNull
+    @NotNull
     @Column(name = "phone_number", length = 9, nullable = false)
     private String phoneNumber;
 
     @Size(min = 1, max = 200)
-    //@NotNull
+    @NotNull
     @Column(name = "description", length = 200, nullable = false)
     private String description;
 
     @Size(min = 1, max = 200)
-    //@NotNull
+    @NotNull
     @Column(name = "location", length = 200, nullable = false)
     private String location;
 
     @Size(min = 11, max = 11)
-    //@NotNull
+    @NotNull
     @Column(name = "ruc", length = 11, nullable = false)
     private String ruc;
 
     @Size(min = 1, max = 500)
-    //@NotNull
+    @NotNull
     @Column(name = "photo", length = 500)
     private String photo;
 
@@ -58,7 +59,8 @@ public class Agency {
     @Column(name = "score", nullable = false)
     private Float score = 0f;
 
-    @OneToMany(mappedBy = "agency", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JsonIgnoreProperties("agency")
+    @OneToMany(mappedBy = "agency", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties({"agency", "activities"})
     private List<Tour> tours;
+
 }
